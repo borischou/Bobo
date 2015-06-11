091fbb0488c4a7fc7dc97628fb6e60421f82b904
Zhouboli <zhouboli@ZhoubolideMacBook-Air.local>
2015-06-11 14:05:30 +0800

    Initial Commit

---
+++ Bobo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bobo
+//
+//  Created by Zhouboli on 15/6/11.
+//  Copyright (c) 2015年 Zhouboli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Bobo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bobo
+//
+//  Created by Zhouboli on 15/6/11.
+//  Copyright (c) 2015年 Zhouboli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
