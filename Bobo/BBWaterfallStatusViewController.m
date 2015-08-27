9785140787e6601470051d82d668dc96b1ec8662
Boris <zhouboli@gmail.com>
2015-08-27 18:25:48 +0800

    cleaned it up and image bug not fixed

--- Bobo/BBWaterfallStatusViewController.m
+++ Bobo/BBWaterfallStatusViewController.m
@@ -40,7 +40,7 @@
     CHTCollectionViewWaterfallLayout *layout = [[CHTCollectionViewWaterfallLayout alloc] init];
     layout.minimumColumnSpacing = 1.0;
     layout.minimumInteritemSpacing = 2.0;
-    layout.itemRenderDirection = CHTCollectionViewWaterfallLayoutItemRenderDirectionLeftToRight;
+    layout.itemRenderDirection = CHTCollectionViewWaterfallLayoutItemRenderDirectionShortestFirst;
     
     _waterfallView = [[BBWaterfallCollectionView alloc] initWithFrame:CGRectMake(0, 0, bWidth, bHeight) collectionViewLayout:layout];
     [self.view addSubview:_waterfallView];
