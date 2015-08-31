9b73e2645ff6bc5dcc8c6bd56be43ee3920493b5
Boris <zhouboli@gmail.com>
2015-08-31 22:59:55 +0800

    added notificaion view class

---
+++ Bobo/BBNotificationView.h
@@ -0,0 +1,13 @@
+//
+//  BBNotificationView.h
+//  Bobo
+//
+//  Created by Boris Chow on 8/31/15.
+//  Copyright (c) 2015 Zhouboli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBNotificationView : UIView
+
+@end
---
+++ Bobo/BBNotificationView.m
@@ -0,0 +1,45 @@
+//
+//  BBNotificationView.m
+//  Bobo
+//
+//  Created by Boris Chow on 8/31/15.
+//  Copyright (c) 2015 Zhouboli. All rights reserved.
+//
+
+#import "BBNotificationView.h"
+
+@implementation BBNotificationView
+
+-(instancetype)init
+{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+-(instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+    }
+    return self;
+}
+
+-(instancetype)initWithNotification:(NSString *)text
+{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+-(void)setupNotification
+{
+    
+}
+
+@end
