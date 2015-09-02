ff593ecc1257b5ea5d2a6f71f303d1e4c0255a16
Boris <zhouboli@gmail.com>
2015-09-02 15:26:01 +0800

    changed some basic data length

--- Bobo/AppDelegate.m
+++ Bobo/AppDelegate.m
@@ -101,7 +101,7 @@
 
 -(BOOL)checkLoginStatus
 {
-    _isLoggedIn = [[NSUserDefaults standardUserDefaults] objectForKey:@"loginstatus"];
+    _isLoggedIn = [[[NSUserDefaults standardUserDefaults] objectForKey:@"loginstatus"] boolValue];
     _wbToken = [[NSUserDefaults standardUserDefaults] objectForKey:@"wbtoken"];
     _wbCurrentUserID = [[NSUserDefaults standardUserDefaults] objectForKey:@"userID"];
     NSLog(@"self.wbCurrentUserID: %@\nself.wbToken: %@", _wbCurrentUserID, _wbToken);
