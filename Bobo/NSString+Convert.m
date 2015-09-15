070443500aea9654b64130486cc2ff025def5453
Boris <zhouboli@gmail.com>
2015-09-15 13:38:48 +0800

    improved regex

--- Bobo/NSString+Convert.m
+++ Bobo/NSString+Convert.m
@@ -63,7 +63,7 @@
 
 +(NSAttributedString *)markedText:(NSString *)text fontSize:(CGFloat)fontSize fontColor:(UIColor *)color
 {
-    NSString *pattern = @"(@([\\w-]+[\\w-]*))|((http://((\\w)+).((\\w)+))+/(\\w)+)|(#[^#]+#)";
+    NSString *pattern = @"(@([\\w-]+[\\w-]*))|((https?://((\\w)+).((\\w)+))+/(\\w)+)|(#[^#]+#)";
     
     NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
     [paragraphStyle setLineSpacing:2.0];
