4a0eff2fa8da38ee715015950746a2b67f30c2ad
Boris <zhouboli@gmail.com>
2015-09-15 23:35:17 +0800

    cleaned it up

--- Bobo/BBStatusTableViewCell.m
+++ Bobo/BBStatusTableViewCell.m
@@ -156,7 +156,6 @@
     _tweetTextLabel.numberOfLines = 0;
     _tweetTextLabel.lineBreakMode = NSLineBreakByWordWrapping;
     _tweetTextLabel.backgroundColor = [UIColor clearColor];
-    _tweetTextLabel.textAlignment = NSTextAlignmentLeft;
     [_tweetTextLabel setAttributes:[Utils genericAttributesWithFontSize:fontSize fontColor:[UIColor customGray]]];
     [_tweetTextLabel setAttributes:[Utils genericAttributesWithFontSize:fontSize fontColor:[UIColor dodgerBlue]] hotWord:STTweetLink];
     [_tweetTextLabel setAttributes:[Utils genericAttributesWithFontSize:fontSize fontColor:[UIColor dodgerBlue]] hotWord:STTweetHashtag];
@@ -194,7 +193,6 @@
     _retweetTextLabel.numberOfLines = 0;
     _retweetTextLabel.lineBreakMode = NSLineBreakByWordWrapping;
     _retweetTextLabel.backgroundColor = [UIColor clearColor];
-    _retweetTextLabel.textAlignment = NSTextAlignmentLeft;
     [_retweetTextLabel setAttributes:[Utils genericAttributesWithFontSize:fontSize fontColor:[UIColor customGray]]];
     [_retweetTextLabel setAttributes:[Utils genericAttributesWithFontSize:fontSize fontColor:[UIColor dodgerBlue]] hotWord:STTweetLink];
     [_retweetTextLabel setAttributes:[Utils genericAttributesWithFontSize:fontSize fontColor:[UIColor dodgerBlue]] hotWord:STTweetHashtag];
