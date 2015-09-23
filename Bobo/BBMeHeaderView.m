7fe27367837e494807abb291325014e4231034f0
Boris <zhouboli@gmail.com>
2015-09-23 21:25:46 +0800

    added scroll pages

--- Bobo/BBMeHeaderView.m
+++ Bobo/BBMeHeaderView.m
@@ -17,36 +17,57 @@
 #define bMaleColor [UIColor colorWithRed:0.0/255 green:154.0/255 blue:205.0/255 alpha:1.0] //light blue
 #define bFemaleColor [UIColor colorWithRed:255.0/255 green:52.0/255 blue:181.0/255 alpha:1.0] //pink
 
+@interface BBMeHeaderView () <UIScrollViewDelegate>
+
+@property (strong, nonatomic) UIScrollView *scrollView;
+@property (strong, nonatomic) UIPageControl *pageControl;
+
+@end
+
 @implementation BBMeHeaderView
 
 -(id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
     if (self) {
-        [self setHeaderLayout];
+        [self setupScrollViewWithFrame:frame];
+        [self loadPageControlWithFrame:frame];
+        [self setHeaderLayoutWithFrame:frame];
     }
     return self;
 }
 
--(void)setHeaderLayout
+-(void)setupScrollViewWithFrame:(CGRect)frame
 {
-    _avatarView = [[UIImageView alloc] initWithFrame:CGRectMake(self.center.x-bWidth/10, 15, bWidth/5, bWidth/5)];
+    _scrollView = [[UIScrollView alloc] initWithFrame:frame];
+    [_scrollView setPagingEnabled:YES];
+    [_scrollView setContentSize:CGSizeMake(bWidth*2, frame.size.height)];
+    [_scrollView setDelegate:self];
+    [_scrollView setShowsHorizontalScrollIndicator:NO];
+    [_scrollView setAlwaysBounceHorizontal:YES];
+    [_scrollView setAlwaysBounceVertical:NO];
+    [self addSubview:_scrollView];
+}
+
+-(void)setHeaderLayoutWithFrame:(CGRect)frame
+{
+    _avatarView = [[UIImageView alloc] initWithFrame:CGRectMake(frame.size.width/2-bWidth/10, 15, bWidth/5, bWidth/5)];
     _avatarView.layer.masksToBounds = YES;
     _avatarView.layer.cornerRadius = _avatarView.bounds.size.width*0.5;
     _avatarView.layer.borderWidth = 0.1;
     _avatarView.layer.borderColor = [UIColor blackColor].CGColor;
-    [self addSubview:_avatarView];
+    [_scrollView addSubview:_avatarView];
     
     _name = [[UILabel alloc] initWithFrame:CGRectMake(0, 15+bWidth/5+10, bWidth, 30)];
     _name.textAlignment = NSTextAlignmentCenter;
-    [self addSubview:_name];
+    [_scrollView addSubview:_name];
     
     _vipView = [[UIImageView alloc] initWithFrame:CGRectZero];
-    [self addSubview:_vipView];
+    [_scrollView addSubview:_vipView];
     
     _introduction = [[UILabel alloc] initWithFrame:CGRectMake(10, 15+bWidth/5+10+30+10, bWidth-20, 30)];
     [_introduction setTextAlignment:NSTextAlignmentCenter];
-    [self addSubview:_introduction];
+    [_scrollView addSubview:_introduction];
 }
 
 -(void)layoutSubviews
@@ -74,6 +95,32 @@
         _vipView.image = nil;
         [_vipView setFrame:CGRectZero];
         _introduction.attributedText = [[NSAttributedString alloc] initWithString:_user.user_description attributes:@{NSFontAttributeName: [UIFont systemFontOfSize:[Utils fontSizeForStatus]], NSForegroundColorAttributeName: [UIColor customGray]}];
+    }
+}
+
+-(void)loadPageControlWithFrame:(CGRect)frame
+{
+    _pageControl = [[UIPageControl alloc] init];
+    _pageControl.bounds = CGRectMake(0, 0, bWidth/2, 20);
+    _pageControl.center = CGPointMake(bWidth/2, frame.size.height-10);
+    _pageControl.numberOfPages = 2;
+    _pageControl.userInteractionEnabled = NO;
+    _pageControl.pageIndicatorTintColor = [UIColor darkGrayColor];
+    _pageControl.currentPageIndicatorTintColor = [UIColor lightTextColor];
+    _pageControl.currentPage = _scrollView.contentOffset.x/bWidth-1;
+    [self addSubview:_pageControl];
+}
+
+#pragma mark - UIScrollViewDelegate
+
+-(void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
+{
+    if (scrollView.contentOffset.x == 0) {
+        scrollView.contentOffset = CGPointMake(0, 0);
+        _pageControl.currentPage = 0;
+    } else if (scrollView.contentOffset.x == bWidth) {
+        _pageControl.currentPage = 1;
+        [scrollView setContentOffset:CGPointMake(bWidth, 0) animated:YES];
     }
 }
 
