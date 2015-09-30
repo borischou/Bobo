6777436ea856669fe713b9d47dbfffa2fb7eeaa9
Boris <zhouboli@gmail.com>
2015-09-30 20:11:56 +0800

    good

--- Pods/MJRefresh/MJRefresh/Base/MJRefreshHeader.m
+++ Pods/MJRefresh/MJRefresh/Base/MJRefreshHeader.m
@@ -45,7 +45,7 @@
     [super placeSubviews];
     
     // 设置y值(当自己的高度发生改变了，肯定要重新调整Y值，所以放到placeSubviews方法中设置y值)
-    self.mj_y = - self.mj_h;
+    self.mj_y = - self.mj_h - self.ignoredScrollViewContentInsetTop;
 }
 
 - (void)scrollViewContentOffsetDidChange:(NSDictionary *)change
@@ -67,11 +67,13 @@
     CGFloat happenOffsetY = - self.scrollViewOriginalInset.top;
     
     // 如果是向上滚动到看不见头部控件，直接返回
-    if (offsetY >= happenOffsetY) return;
+    // >= -> >
+    if (offsetY > happenOffsetY) return;
     
     // 普通 和 即将刷新 的临界点
     CGFloat normal2pullingOffsetY = happenOffsetY - self.mj_h;
     CGFloat pullingPercent = (happenOffsetY - offsetY) / self.mj_h;
+    
     if (self.scrollView.isDragging) { // 如果正在拖拽
         self.pullingPercent = pullingPercent;
         if (self.state == MJRefreshStateIdle && offsetY < normal2pullingOffsetY) {
@@ -106,7 +108,7 @@
             self.scrollView.mj_insetT -= self.mj_h;
             
             // 自动调整透明度
-            if (self.isAutoChangeAlpha) self.alpha = 0.0;
+            if (self.isAutomaticallyChangeAlpha) self.alpha = 0.0;
         } completion:^(BOOL finished) {
             self.pullingPercent = 0.0;
         }];
