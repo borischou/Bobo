a0a9b2b42a1aeec3d31cd7b21ddde39af9dbd205
Boris <zhouboli@gmail.com>
2015-10-03 13:21:40 +0800

    moving

--- Bobo/BBProfileMenuHeaderView.m
+++ Bobo/BBProfileMenuHeaderView.m
@@ -14,14 +14,24 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        
+        [self setupSubviews];
     }
     return self;
 }
 
 -(void)setupSubviews
 {
-    
+    UIButton *photoButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [photoButton setTitle:@"Album" forState:UIControlStateNormal];
+    [photoButton setFrame:CGRectMake(0, 0, self.frame.size.width/3, self.frame.size.height)];
+    [photoButton.titleLabel setFont:[UIFont systemFontOfSize:13.0]];
+    [photoButton addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    [self addSubview:photoButton];
+}
+
+-(void)buttonPressed:(UIButton *)sender
+{
+
 }
 
 @end
