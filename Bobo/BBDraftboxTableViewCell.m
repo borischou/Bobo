ecb6f4ca08f388772e1e52b51f24d428e906e72e
Boris <zhouboli@gmail.com>
2015-10-13 09:55:17 +0800

    movings a little slow

--- Bobo/BBDraftboxTableViewCell.m
+++ Bobo/BBDraftboxTableViewCell.m
@@ -8,13 +8,16 @@
 
 #import "BBDraftboxTableViewCell.h"
 
+static CGFloat dSmallGap = 5;
+static CGFloat dBigGap = 10;
+
 @interface BBDraftboxTableViewCell ()
 
 @property (strong, nonatomic) UIButton *resendButton; //重新发送按钮
 @property (strong, nonatomic) UILabel *type; //草稿类型
 @property (strong, nonatomic) UILabel *content; //草稿内容
 @property (strong, nonatomic) UILabel *time; //草稿保存时间
-@property (strong, nonatomic) UIImageView *preview; //草稿第一张附图预览
+@property (strong, nonatomic) UIImageView *preview; //草稿第一张附图预览(如果有)
 
 //转发草稿
 @property (strong, nonatomic) UILabel *repostContent; //原微博文本
