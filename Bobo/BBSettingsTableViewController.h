d15f688a5e65f0e5f007d16ddbbaa7e0188ba75d
Boris <zhouboli@gmail.com>
2015-10-16 23:06:16 +0800

    added settings

---
+++ Bobo/BBSettingsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  BBSettingsTableViewController.h
+//  Bobo
+//
+//  Created by Boris Chow on 10/16/15.
+//  Copyright © 2015 Zhouboli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBSettingsTableViewController : UITableViewController
+
+@end
